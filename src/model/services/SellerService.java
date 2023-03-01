6d4a45a6edf93aebeb5af3ef4418456a91c2a7ee
Emerson Veloso <emesonveloso.s@gmail.com>
2023-03-01 20:39:37 -0300

    Saving Seller

--- src/model/services/SellerService.java
+++ src/model/services/SellerService.java
@@ -9,20 +9,19 @@ import model.entities.Seller;
 public class SellerService {
 
 	private SellerDao dao = DaoFactory.createSellerDao();
-	
+
 	public List<Seller> findAll() {
 		return dao.findAll();
 	}
-	
+
 	public void saveOrUpdate(Seller obj) {
 		if (obj.getId() == null) {
 			dao.insert(obj);
-		}
-		else {
+		} else {
 			dao.update(obj);
 		}
 	}
-	
+
 	public void remove(Seller obj) {
 		dao.deleteById(obj.getId());
 	}
